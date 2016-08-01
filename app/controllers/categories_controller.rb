5d5c5466ba50aff1151eb3251417fad377d6409a
Andrew Peterson & Anna Krupp <pair+ajpeterson.cherchezlafemme@devbootcamp.com>
2016-08-01 11:29:15 -0700

    Add show action to controller and show view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,23 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
