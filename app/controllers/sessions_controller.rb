151656bac84d684bd6102250f308e18d5a61b61a
Andrew Peterson & Anna Krupp <pair+ajpeterson.cherchezlafemme@devbootcamp.com>
2016-08-01 16:56:28 -0700

    Fill application helper code with methods that helps us to authenticate user and look up the current sessions. Buttons on layout page are changing depending if the user is login and not

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,36 @@ class SessionsController < ApplicationController
 
   def new
     @user = User.new
-
   end
 
   def create
-    @user = User.new(user_params)
-
-
+    @user = User.find_by_email(email_params)
+    if @user && @user.authenticate(auth_params)
+      authentication_login(@user)
+      redirect_to root_path, :notice => "Welcome back, #{@user.username}"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
 
   def destroy
+    authentication_logout
+    redirect_to '/users/new'
+  end
 
+  private
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def email_params
+    login_params[:email]
+  end
+
+  def auth_params
+    login_params[:password]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def authentication_logged_in?
+    !!session[:user_id]
+  end
+
+  private
+  def authentication_login(user)
+    session[:user_id] = user.id
+  end
+
+  def authentication_logout
+     session.clear
+  end
+
+  def check_access_to_restricted_area
+    if authentication_logged_in?
+
+    else
+      flash_set('Access Denied Please Login')
+      redirect '/'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
