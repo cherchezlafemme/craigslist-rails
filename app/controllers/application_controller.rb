176c64cc683a693f119c207d362f9e195aff2b23
Andrew Peterson & Anna Krupp <pair+ajpeterson.cherchezlafemme@devbootcamp.com>
2016-08-01 15:55:21 -0700

    Initialized sessions controller, and users controller with some of the corresponding views.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,4 +28,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
